c0581e6df1907e0b514cd61564f1fe6b3deea920
bartswierz <bartoszswierzynski7@gmail.com>
2022-09-16 00:08:47 -0500

    fixed import pathing for Home route

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
 // import CategoryItem from "./components/category-item/category-item.component";
-import Directory from "./components/directory/directory.component";
+// import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
+// import Home from "./routes/home/home.component";
+import Home from "./components/routes/home/home.component";
+// components\routes\home\home.component
 /* If text is the SAME for all areas, we can hardcode those,
 If text is different, then we PUT THOSE IN THE OBJECT CATEGORIES
 - categories.map((category) => {container content}. In this, we are going to iterate through our categories array to input out each of the names. This helps to PREVENT DRY.
   */
 const App = () => {
-  // THIS IS OUR JSON
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  // <Routes>
+  //   <Route path="/" index element={<Home />} />
+  // </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
+
+// element RENDERS IN our HOME COMPONENT
+{
+  /* <Route path="/" element={<Home />} /> */
+}
 
 /* Passing in category object as a prop into our category-item component
 id MUST BE CALLED HERE where MAP is USED. NOT INSIDE THE COMPONENT
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
