67252f64df6fdf1987214e67f1639c7599e4f2a3
bartswierz <bartoszswierzynski7@gmail.com>
2022-10-01 01:40:41 -0500

    added cart dropdown logic, currently adds product name and quantity inside dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,17 @@
-import "./product-card.styles.scss";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
+
+import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   // Destructuring off what we need from passed 'product'
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -13,7 +20,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,57 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+  // If found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+    );
+  }
+
+  // return new array with modified cartItems/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+// Like a GLOBAL VARIABLE, can use this anywhere else(that is wrapped in the <CartProvider />)
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+
+/*
+product
+{
+  id,
+  name,
+  price,
+  imageUrl
+}
+
+We want our Cart Item to ALSO have 'quantity'
+Cart Item
+{
+  id,
+  name,
+  price,
+  imageUrl,
+  quantity
+}
+*/
